af768b18b23b95c9fd3ab2af754b95864ebc6355
Your Name <you@example.com>
2019-05-22 13:26:38 +0300

    Did my best,sorry:(

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:description, :image, :user_id)
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_path(current_user)
+  end
+end
